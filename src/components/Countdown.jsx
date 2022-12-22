1542ce9e6a116c3f1a9f1fd52367b0419cbe43d1
Hope143 <higoyvictor143@gmail.com>
2022-12-22 19:57:46 +0800

    remove countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import DateCountdown from 'react-date-countdown-timer';
-
-
+import React from 'react'
 
 function Countdown() {
   return (
-    <div className='countdownContainer m-auto p-5 w-100 position-relative'>
-      <h1 className='textHeader text-center mb-0'>Countdown</h1>
-      <img className='dividingPattern d-flex mx-auto mb-5' width={300}  src="/images/dividing-pattern-1.png" alt="photo" />
-      <div className='text-center fs-1 fw-semibold'>
-        <DateCountdown dateTo='December 30, 2022 00:00:00 GMT+03:00' callback={()=>alert('Hello')}  />
-      </div>
-    </div>
+    <div>Countdown</div>
   )
 }
 
