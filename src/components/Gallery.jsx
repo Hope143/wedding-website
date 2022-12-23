ca1178c6a6713b6737ae81c4c5dd04b0caa8df32
Hope143 <higoyvictor143@gmail.com>
2022-12-23 11:57:41 +0800

    changes pic location

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -14,38 +14,38 @@ const Gallery = () => {
             <img className='dividingPattern d-flex mx-auto mb-5 mb-sm-4' width={300}  src="/images/dividing-pattern-1.png" alt="photo" />
           </div>
         </div>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../../public/prenupImg/picture1.jpg" class="d-block w-100" alt="..."/>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="../prenupImg/picture1.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item">
-      <img src="../../public/prenupImg/picture2.jpg" class="d-block w-100" alt="..."/>
+    <div className="carousel-item">
+      <img src="../prenupImg/picture2.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item">
-      <img src="../../public/prenupImg/picture3.jpg" class="d-block w-100" alt="..."/>
+    <div className="carousel-item">
+      <img src="../prenupImg/picture3.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item">
-      <img src="../../public/prenupImg/picture4.jpg" class="d-block w-100" alt="..."/>
+    <div className="carousel-item">
+      <img src="../prenupImg/picture4.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item">
-      <img src="../../public/prenupImg/picture5.jpg" class="d-block w-100" alt="..."/>
+    <div className="carousel-item">
+      <img src="../prenupImg/picture5.jpg" className="d-block w-100" alt="..."/>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
 </div>
     </div>
