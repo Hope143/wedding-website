cd25b8e0c10414cd96b4147068e30059f88fd279
Hope143 <higoyvictor143@gmail.com>
2022-12-23 19:49:33 +0800

    changes the font again

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -85,7 +85,7 @@ function FrontPage() {
       />
         <div className="text-Container">
         <h1 className="titleHeader mb-3 d-lg-flex align-items-end d-none">
-          Imman <span className="andHeader px-4">&</span> Em
+          IMMAN <span className="andHeader px-4">&</span> EM
         </h1>
         <h1 className="titleHeader mb-3 d-flex text-center d-lg-none align-items-end">
         Imman & Em
