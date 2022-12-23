85d4c4e1c29e5bd4a3c984b6b354278b3240ca42
Hope143 <higoyvictor143@gmail.com>
2022-12-23 13:53:44 +0800

    changes the arrow in gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -44,11 +44,11 @@ const Gallery = () => {
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="bx bx-chevron-left" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
   <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="bx bx-chevron-right" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
 </div>
